5402a81a680e9cc1093e9b732424ddcc37ab9c96
rei0ier <139188412+rei0ier@users.noreply.github.com>
2023-08-11 15:05:43 +0900

    Unityちゃん走るスクリプト
    Lesson6　C4.4

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+    //アニメーションするためのコンポーネントを入れる
+    private Animator myAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Animatorコンポーネントを取得
+        this.myAnimator = GetComponent<Animator>();
+
+        //走るアニメーション
+        this.myAnimator.SetFloat("Speed", 1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
