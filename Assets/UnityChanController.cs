d354b8e92d7c023e4d3be23f896d4659cdd0f832
rei0ier <139188412+rei0ier@users.noreply.github.com>
2023-08-12 23:13:59 +0900

    Jump設定
    Lesson 6Chapter 4.9-4.12

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -13,6 +13,8 @@ public class UnityChanController : MonoBehaviour
     private float velocityZ = 16f;
     //横方向の速度
     private float velocityX = 10f;
+    //上方向の速度
+    private float velocityY = 10f;
     //左右の移動できる範囲
     private float movableRange = 3.4f;
 
@@ -33,23 +35,44 @@ public class UnityChanController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //横方向の入力による速度（追加）
+        //横方向の入力による速度
         float inputVelocityX = 0;
+        //上方向の入力による速度
+        float inputVelocityY = 0;
 
-        //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる（追加）
+        //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる
         if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
         {
-            //左方向への速度を代入（追加）
+            //左方向への速度を代入
             inputVelocityX = -this.velocityX;
         }
         else if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
         {
-            //右方向への速度を代入（追加）
+            //右方向への速度を代入
             inputVelocityX = this.velocityX;
+        }
+        //ジャンプしていない時にスペースキーが押されたらジャンプする
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            //ジャンプアニメを再生
+            this.myAnimator.SetBool("Jump", true);
+            //上方向への速度絵を代入
+            inputVelocityY = this.velocityY;
+        }
+        else
+        {
+            //現在のY軸の速度を代入
+            inputVelocityY = this.myRigidbody.velocity.y;
+        }
+
+        //Junpステートの場合はJunpにfalseをセットする
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
         }
 
         //Unityちゃんに速度を与える
-        this.myRigidbody.velocity = new Vector3(inputVelocityX, 0, velocityZ);
+        this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, velocityZ);
 
     }
 }
